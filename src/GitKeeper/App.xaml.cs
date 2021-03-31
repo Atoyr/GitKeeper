4be53a0f1247aa568e3d267313e9deed6bc7745f
Atoyr <lessironglance@gmail.com>
2021-03-31 09:36:05 +0900

    init wpf application

---
+++ src/GitKeeper/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Prism.Ioc;
+using Prism.Unity;
+using Prism.Modularity;
+using Prism.Regions;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using MahApps.Metro.Theming;
+using ControlzEx.Theming;
+using Unity;
+
+namespace GitKeeper
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : PrismApplication
+  {
+    protected override Window CreateShell()
+    {
+      return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry) 
+    {
+      containerRegistry.RegisterInstance<IDialogCoordinator>(DialogCoordinator.Instance);
+    }
+
+    protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+    {
+      base.ConfigureModuleCatalog(moduleCatalog);
+      moduleCatalog.AddModule(typeof(Module));
+    }
+
+    protected override IModuleCatalog CreateModuleCatalog()
+    {
+      return new ConfigurationModuleCatalog();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+      base.OnStartup(e);
+
+      // Add custom theme resource dictionaries
+      // You should replace SampleApp with your application name
+      // and the correct place where your custom theme lives
+      var theme = ThemeManager.Current.AddLibraryTheme(
+          new LibraryTheme(
+            new Uri("pack://application:,,,/GitKeeper;component/Styles/Themes/Dark.Default.xaml"),
+            MahAppsLibraryThemeProvider.DefaultInstance
+            )
+          );
+
+      ThemeManager.Current.ChangeTheme(this, theme);
+    }
+
+  }
+}
---
+++ src/GitKeeper/MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Prism.Mvvm;
+using Prism.Regions;
+using Prism.Events;
+using Prism.Unity;
+using Unity;
+using Prism.Services.Dialogs;
+using Prism.Commands;
+using GitKeeper.ViewModels;
+using Reactive.Bindings;
+
+namespace GitKeeper
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public ReactiveProperty<string> Title { get; } = new ReactiveProperty<string>();
+
+    public override void Initialize(IUnityContainer unityContainer)
+    {
+      this.Title.Value = "GitKeeper";
+    }
+  }
+}
+
+
