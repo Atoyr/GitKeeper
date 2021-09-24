513e7fbf43b3fe957d7970f282abafbaa33b7031
Atoyr <lessironglance@gmail.com>
2021-09-24 09:49:52 +0900

    update colors namespace

---
+++ src/Colors/Colors.cs
@@ -0,0 +1,317 @@
+using System;
+using System.IO;
+
+namespace GitKeeper
+{
+    public static partial class Colors
+    {
+        public static partial class Red
+        {
+            public const string Default = "red";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Pink
+        {
+            public const string Default = "pink";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Purple
+        {
+            public const string Default = "purple";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class DeepPurple
+        {
+            public const string Default = "deep-purple";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Indigo
+        {
+            public const string Default = "indigo";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Blue
+        {
+            public const string Default = "blue";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Green
+        {
+            public const string Default = "green";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class LightGreen
+        {
+            public const string Default = "light-green";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Lime
+        {
+            public const string Default = "lime";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Yellow
+        {
+            public const string Default = "yellow";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Amber
+        {
+            public const string Default = "amber";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Orange
+        {
+            public const string Default = "orange";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class DeepOrange
+        {
+            public const string Default = "deep-orange";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Brown
+        {
+            public const string Default = "brown";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class BlueGray
+        {
+            public const string Default = "blue-gray";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static partial class Gray
+        {
+            public const string Default = "gray";
+            public const string Lighten5 = Default + "-lighten-5";
+            public const string Lighten4 = Default + "-lighten-4";
+            public const string Lighten3 = Default + "-lighten-3";
+            public const string Lighten2 = Default + "-lighten-2";
+            public const string Lighten1 = Default + "-lighten-1";
+            public const string Darken1 = Default + "-darken-1";
+            public const string Darken2 = Default + "-darken-2";
+            public const string Darken3 = Default + "-darken-3";
+            public const string Darken4 = Default + "-darken-4";
+            public const string Accent1 = Default + "-accent-1";
+            public const string Accent2 = Default + "-accent-2";
+            public const string Accent3 = Default + "-accent-3";
+            public const string Accent4 = Default + "-accent-4";
+        }
+
+        public static class Shades
+        {
+            public const string Black = "black";
+            public const string White = "white";
+            public const string Transparent = "transparent";
+
+            public const string Darken80 = "darken-80";
+            public const string Darken60 = "darken-60";
+            public const string Darken40 = "darken-40";
+            public const string Darken20 = "darken-20";
+            public const string Lighten80 = "lighten-80";
+            public const string Lighten60 = "lighten-60";
+            public const string Lighten40 = "lighten-40";
+            public const string Lighten20 = "lighten-20";
+        }
+
+        public static string Foreground(string color) => "fg-" + color;
+        public static string Background(string color) => "bg-" + color;
+        public static string HoverForeground(string color) => "hover-fg-" + color;
+        public static string HoverBackground(string color) => "hover-bg-" + color;
+    }
+}
