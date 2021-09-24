98466958c9e268c660eba9420a06f6cc5c402827
Atoyr <lessironglance@gmail.com>
2021-09-25 07:52:47 +0900

    update appconfig

--- src/AppConfig.cs
+++ src/AppConfig.cs
@@ -4,15 +4,43 @@ namespace GitKeeper
 {
     public class AppConfig
     {
-        private string appName { get; set;  }
+        const string USERPROFILE = "USERPROFILE";
+        const string ConfigFileName = "config";
+        const string ThemeFolderName = "Theme";
 
-        // public string ApplicationPath { get; } = System.Reflection.Assembly.GetExecutingAssembly().Location;
+        public string AppName { get; private set; }
+        public string Path { get; private set; }
+        public string ThemeDirectory
+        {
+            get => System.IO.Path.Combine(Path, ThemeFolderName);
+        }
+
 
         private AppConfig() { }
 
         public AppConfig(string appName)
         {
-            this.appName = appName;
+            AppName = appName;
+            Initialize();
+        }
+
+        protected void Initialize()
+        {
+            Path = System.IO.Path.Combine(Environment.GetEnvironmentVariable(USERPROFILE), "." + AppName);
+            if (!System.IO.Directory.Exists(Path))
+            {
+                System.IO.Directory.CreateDirectory(Path);
+            }
+
+            ThemeInitialize();
+        }
+
+        protected void ThemeInitialize()
+        {
+            if (!System.IO.Directory.Exists(ThemePath))
+            {
+                System.IO.Directory.CreateDirectory(ThemePath);
+            }
         }
     }
 }
